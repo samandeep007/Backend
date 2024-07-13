871b183e1e618764edc086da6d96b6799ad8b64e
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-13 13:22:04 -0600

    chore: Update @babel/register import and add Mocha import in setupTests.js

--- src/test/setupTests.js
+++ src/test/setupTests.js
@@ -1,15 +1,18 @@
-// Import necessary modules
-require('@babel/register'); // Ensure Babel is registered for transforming code
+// Import @babel/register to transform code
+import '@babel/register';
 
-const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const sinon = require('sinon'); // Import Sinon for mocking
+// Import necessary modules
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import sinon from 'sinon'; // Import Sinon for mocking
+import Mocha from 'mocha';
 
 let mongoServer;
 
+
 // Mocha hooks for setup and teardown
-before(async function () {
-    this.timeout(60000); // Increase timeout to ensure MongoMemoryServer has enough time to start
+before(async () => {
+    this.timeout(60000);
     mongoServer = await MongoMemoryServer.create();
     const uri = mongoServer.getUri();
     await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
