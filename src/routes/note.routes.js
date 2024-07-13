2269b8abafea2532ed7c02ac38e21bf7faf00ae6
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-12 23:10:07 -0600

    Refactor note.routes.js for improved code organization and readability

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,2 +1,30 @@
-
+import express from 'express';
+import {
+    createNote,
+    getCurrentNote,
+    getAllNotes,
+    updateNote,
+    deleteNote,
+    shareNote,
+    searchNotes
+} from '../controllers/notes.controller.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
+
+
+router.route('/api/notes')
+    .get(verifyJWT, getAllNotes)
+    .post(verifyJWT, createNote);
+
+router.route('/api/notes/:noteId')
+    .get(verifyJWT, getCurrentNote)
+    .put(verifyJWT, updateNote)
+    .delete(verifyJWT, deleteNote);
+
+router.route('/api/notes/:id/share')
+    .post(verifyJWT, shareNote);
+
+
+router.route('/api/search')
+    .get(verifyJWT, searchNotes);
+
+export default router;
