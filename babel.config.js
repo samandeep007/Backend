9acc2d49d283b08de441f8611882ab7807510f19
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-13 09:14:38 -0600

    chore: Update Babel configuration and package.json scripts for improved testing capabilities

--- babel.config.js
+++ babel.config.js
@@ -1,2 +1,4 @@
-export const presets = ['@babel/preset-env'];
+module.exports = {
+    presets: ['@babel/preset-env'],
+  };
   
---
+++ src/tests/setupBabel.js
@@ -0,0 +1,5 @@
+require('@babel/register')({
+    ignore: [/(node_module)/],
+    extensions: ['.js'],
+  });
+  
