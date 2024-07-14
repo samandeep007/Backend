355038611c73ceee2685716aa02d028194665797
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-14 02:17:55 -0600

    refactor: Update Swagger server URL in swagger.js

--- src/swagger.js
+++ src/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: `${process.env.BASE_URL}`, // Change this to your server's URL
+        url: process.env.BASE_URL, // Change this to your server's URL
       },
     ],
   },
